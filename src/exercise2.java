2da1f437b2a817b8f38c02abde1144c992279ab3
Avihay Ben Ami <avihaybg3@gmail.com>
2022-11-17 13:52:52 +0200

    1

--- src/exercise2.java
+++ src/exercise2.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class exercise2 {
         public static void main(String []args){
-            int firstnumber;
+            int firstnumber; //variables for parameters.
             int diffrence;
             int elements;
             Scanner scanner=new Scanner(System.in);
@@ -11,14 +11,14 @@ public class exercise2 {
             diffrence=scanner.nextInt();
             System.out.println("Enter Number of elements");
             elements=scanner.nextInt();
-            function1(firstnumber,diffrence,elements);
+            function1(firstnumber,diffrence,elements); //go to function with variables values.
         }
         public static void function1(int firstnumber,int diffrence,int elements){
-            int number=firstnumber;
-            System.out.print("the series math is " +firstnumber+" ");
-            for(int i=1; i<elements; i++){
-                System.out.print(number+diffrence+" ");
-                number=number+diffrence;
+            int number=firstnumber; //initialization the print number to first number.
+            System.out.print("the series math is " +firstnumber+" "); //print just the first number.
+            for(int i=1; i<elements; i++){ //loop from 1 until numbers of elements.
+                System.out.print(number+diffrence+" "); //print the number+the diffrernce each time until the number of the elements.
+                number=number+diffrence; //add the difference to number for the next loop.
             }
         }
     }
