1d4635c545052f5d7837575526042e45b8553156
Avihay Ben Ami <avihaybg3@gmail.com>
2022-11-19 16:48:11 +0200

    1

--- src/exercise3.java
+++ src/exercise3.java
@@ -1,19 +1,37 @@
 import java.util.Scanner;
 
 public class exercise3 {
-    public static void main(String []args){
-        Scanner scanner=new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         int number;
-        do{
+        do {
             System.out.println("Enter a odd and positive number bigger than 3");
-            number=scanner.nextInt();}
-        while(!(number%2!=0&&number>= 3));
+            number = scanner.nextInt();
+        }
+        while (!(number % 2 != 0 && number >= 3));
         diamond(number);
     }
-    public static void diamond(int number){
-        for(int i=1; i<=number ;i+=2){
-            for(int j=1;j<=i;j++){
-                System.out.print("*");}
-            System.out.println(" ");}
+
+    public static void diamond(int number) {
+        for (int i = 1; i <= number; i += 2) {
+            for (int j = 1; j <= i; j++) {
+                if(j==1){
+                    for(int k=i; k<=number; k+=2){
+                        System.out.print(" ");}
+                }
+                System.out.print("*");
             }
+            System.out.println(" ");
+        }
+        for (int i = number-2; i >= 1; i -=2) {
+            for (int j = i; j >= 1; j--) {
+                if(j==i){
+                    for(int k=i; k<=number; k+=2){
+                        System.out.print(" ");}
+                }
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
     }
+}
