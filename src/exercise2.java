cc573cb267da9373b32393530301b3bf21e25ec5
Avihay Ben Ami <avihaybg3@gmail.com>
2022-11-23 16:53:24 +0200

    1

--- src/exercise2.java
+++ src/exercise2.java
@@ -9,16 +9,18 @@ public class exercise2 {
             firstnumber=scanner.nextInt();
             System.out.println("Enter The diffrence");
             diffrence=scanner.nextInt();
+            do{
             System.out.println("Enter Number of elements");
-            elements=scanner.nextInt();
+            elements=scanner.nextInt();}
+            while(elements<1);
             function1(firstnumber,diffrence,elements); //go to function with variables values.
         }
-        public static void function1(int firstnumber,int diffrence,int elements){
-            int number=firstnumber; //initialization the print number to first number.
-            System.out.print("the series math is " +firstnumber+" "); //print just the first number.
-            for(int i=1; i<elements; i++){ //loop from 1 until numbers of elements.
-                System.out.print(number+diffrence+" "); //print the number+the diffrernce each time until the number of the elements.
-                number=number+diffrence; //add the difference to number for the next loop.
+        public static void function1(int firstnumber,int diffrence,int elements) {
+            int number = firstnumber; //initialization the print number to first number.
+                System.out.print("the series math is " + firstnumber + " "); //print just the first number.
+                for (int i = 1; i < elements; i++) { //loop from 1 until numbers of elements.
+                    System.out.print(number + diffrence + " "); //print the number+the diffrernce each time until the number of the elements.
+                    number = number + diffrence; //add the difference to number for the next loop.
+                }
             }
         }
-    }
